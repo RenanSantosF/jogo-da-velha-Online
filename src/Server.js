c97ab1b9aa0e9e4cd9c0e2244b9271513b649447
Renan Santos <renan_ferreira.es@outlook.com>
2024-04-09 16:14:30 -0300

    Ajusta detalhes de empate

--- src/Server.js
+++ src/Server.js
@@ -69,20 +69,6 @@ function verificarCampeao(element) {
     element[1][1] == element[2][0]
   ) {
     vencedor = element[0][2];
-  } else {
-    if (
-      element[0][0] !== "" &&
-      element[0][1] !== "" &&
-      element[0][2] !== "" &&
-      element[1][0] !== "" &&
-      element[1][1] !== "" &&
-      element[1][2] !== "" &&
-      element[2][0] !== "" &&
-      element[2][1] !== "" &&
-      element[2][2] !== ""
-    ) {
-      vencedor = "V";
-    }
   }
   else {
     if (
@@ -98,7 +84,6 @@ function verificarCampeao(element) {
     ) {
       vencedor = "V";
     }
-
   }
   return vencedor;
 }
